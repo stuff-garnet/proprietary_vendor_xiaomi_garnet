e26e22384a1aaea48c8486adeb4fcd71acabd4dc
Cosmin Tanislav <demonsingur@gmail.com>
2025-03-26 07:54:24 -0300

    garnet: Rename prebuilt libgrpc++_unsecure.so for QCC
    Change-Id: Ia468e586385a2ce7dd0cfeddded2492cb4d9b776

--- garnet-vendor.mk
+++ garnet-vendor.mk
@@ -1383,7 +1383,7 @@ PRODUCT_PACKAGES += \
     libgnsspps \
     libgps.utils \
     libgpudataproducer \
-    libgrpc++_unsecure \
+    libgrpc++_unsecure_prebuilt \
     libgsl \
     libhdr10plus \
     libhdr_backlight_adapter \
