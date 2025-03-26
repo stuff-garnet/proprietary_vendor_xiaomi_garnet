19c465366eefe5a73388689488e0d4a38d48c487
Bruno Martins <bgcngm@gmail.com>
2025-03-26 07:41:10 -0300

    garnet: Declare EGL libs as symlinks during extraction
    Change-Id: I4b2f3e79b5e518889d9771bf8990e27babca8b61

--- garnet-vendor.mk
+++ garnet-vendor.mk
@@ -2201,3 +2201,8 @@ PRODUCT_PACKAGES += \
     xtwifi-inet-agent \
     qccsyshal@1.2-service \
     wfdservice64
+
+PRODUCT_PACKAGES += \
+    libEGL_adreno_libEGL_adreno_symlink64 \
+    libGLESv2_adreno_libGLESv2_adreno_symlink64 \
+    libq3dtools_adreno_libq3dtools_adreno_symlink64
